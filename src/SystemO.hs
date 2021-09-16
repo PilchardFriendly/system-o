62a65dc3612225c0a6164b63815ee9d7f709326c
Nicholas Drew <code@nickdrew.io>
2021-09-16 17:10:45 +1000

    SYSO-3: we can read an empty openapi spec

--- src/SystemO.hs
+++ src/SystemO.hs
@@ -1,4 +1,23 @@
-module SystemO (someFunc) where
+{-# LANGUAGE TypeApplications #-}
+module SystemO (someFunc,exampleApi) where
+import System.FilePath
+import Data.OpenApi as O
+import Data.Aeson as J
+import Data.Text.Lazy.Encoding
+import Data.Text.Lazy
+import qualified Data.ByteString.Lazy as B
+emptyApi :: FilePath 
+emptyApi =  (takeDirectory $ takeDirectory $ __FILE__) </> "examples/api-empty.yml"
 
 someFunc :: Int
 someFunc = 0
+
+exampleApi :: IO (Maybe Text)
+exampleApi = do
+    bytes <- B.readFile emptyApi
+    pure $ transform <$> parse bytes
+        where
+            parse :: B.ByteString -> Maybe O.OpenApi
+            parse = J.decode
+            transform :: O.OpenApi -> Text
+            transform = decodeUtf8 . J.encode
--- test/SystemOSpec.hs
+++ test/SystemOSpec.hs
@@ -1,9 +1,20 @@
+
 module SystemOSpec(spec) where
 import Test.Hspec (Spec, describe, it, shouldBe)
-import SystemO (someFunc)
+import SystemO (someFunc, exampleApi)
+import Data.Text.Lazy as T
 
 spec :: Spec
 spec = do
-    describe "someFunc" $ do
-        it "is 0" $ do
-            someFunc `shouldBe` 0
+    describe "SystemO" $ do
+        describe "someFunc" $ do
+            it "is 0" $ do
+                someFunc `shouldBe` 0
+        describe "example" $ do
+            it "parses example" $ do
+                actual <- exampleApi 
+                actual `shouldBe` expected
+                where 
+                    expected = Just $ T.fromStrict "{\"openapi\":\"3.0.0\",\"info\":{\"title\":\"\",\"version\":\"\"},\"components\":{}}"
+
+
